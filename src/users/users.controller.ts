69a3660f12c79f7d275d5059bbc56bd7bb301958
Manh luong <manh.luong@amela.vn>
2022-02-28 23:10:59 +0700

    Interceptor

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Get, HttpException, HttpStatus, Param, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, UseGuards, UseInterceptors } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User, Users } from './interfaces/user.interface';
 import { Roles, RoleGuard } from './user.guards';
+import { UserInterceptor } from './user.interceptor';
 
-//Binding Guard
+//Binding Guard, Interceptor for this controller
 @Controller('users')
 @UseGuards(RoleGuard)
+@UseInterceptors(UserInterceptor)
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
